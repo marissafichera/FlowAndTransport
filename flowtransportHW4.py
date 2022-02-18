0afee5d1739740b852968de35829cd6e7b986156
mfichera_workcomp <marissa.fichera@nmt.edu>
2022-02-18 11:17:38 -0700

    annoying

--- flowtransportHW4.py
+++ flowtransportHW4.py
@@ -7,81 +7,99 @@ from matplotlib import cm
 import math
 import os
 
+
 def problem_4_3():
-    L = 1       # tube length (m)
-    Ti = 300        #Initial temp (K)
-    T0l = 320        # temp at t=0, left
-    T0r = 300       # temp at t = 0, right
-    t = np.linspace(0, 100000, 200000)
+    L = 1  # tube length (m)
+    # Ti = 300        #Initial temp (K)
+    T0l = 320  # temp at t=0, left
+    T0r = 300  # temp at t = 0, right
+    # t = np.linspace(0, 100000, 200000)
     x = np.linspace(0, 1, 1000)
     c1 = -20
     c2 = 320
 
-    u = c1*x + c2
+    tinitial = T0r + (T0r - T0r) * x  # uniform initial temp distribution
+
+    T_f = c1 * x + c2
     plt.figure(1)
-    plt.plot(x, u)
+    plt.plot(x, tinitial, label='initial')
+    plt.plot(x, T_f, label='final')
     plt.xlabel('x(m)')
     plt.ylabel('temperature (K)')
-    plt.title('Initial and final equilibrium temperature profile')
+    plt.title('Initial and final equilibrium temperature profiles')
+    plt.legend()
     plt.show()
 
-    Tiold = u
-    K = 0.6     #thermal conductivity of water
+    tfinal = 100
     dt = 0.001
-
-    Tinews = []
-    # for t in np.arange(0, 1000000, dt):
-    #     Tinew = Tiold + K*(u)*t
-    #     Tinews.append(Tinew)
-    # plt.figure(2)
-    # plt.plot(x, Tinews)
-    # plt.show()
-    #
-    # print('Tinew = ', Tinew)
-    p = 1000
-    m = np.zeros((p, 1000))
+    timesteps = np.linspace(0, tfinal, 101)
+    print(timesteps)
+    num_nvals = 10
+    num_tvals = 10
+    # entire matrix of n values for each timestep
+    # m = np.zeros((num_tvals, num_nvals))
+    m = []
+    eterm = np.zeros((num_nvals))
+    print(m)
     ss = []
     # print(np.arange(1, 100000))
-    for n in np.arange(1, 1000):
-        m[n] = (1/n)*np.sin(n*np.pi*x)
-        # print('m = ', m)
-        s = np.sum(m)
-        ss.append(s)
-    print(ss)
-    T_x = T0l + (T0r - T0l)*x + (2/np.pi)*ss*(T0r-T0l)
+    temp_profiles = np.zeros((num_tvals, len(x))
 
-    plt.figure(2)
-    plt.plot(T_x)
+    # summation of sin terms
+
+    K = 0.000000155
+
+    for t in timesteps:
+        print('timestep = ', t)
+        for n in np.arange(1, num_nvals+1):
+            print('n value = ', n)
+            eterm = -K * (n ** 2) * (np.pi ** 2) * t / (L ** 2)
+            print('eterm = ', eterm)
+            sumterm = (1 / n) * np.sin(n * np.pi * x / L) * np.exp(eterm)
+            print('sumterm = ', sumterm)
+            # print(sumterm)
+        # print('SUM = ', np.sum(sumterm))
+        temp = T0l + (T0r - T0l)*(x/L) + (T0r - T0l)*(2/np.pi)*np.sum(sumterm)
+
+        # print('temp at t={} = '.format(t), temp)
+
+        # m[t] = np.sum(sumterm)
+        # print('m = ', m)
+
+    #     # print('m = ', m)
+    #     s = np.sum(m)
+    #     ss.append(s)
+    # print(ss)
+    # T_x = T0l + (T0r - T0l)*x + (2/np.pi)*ss*(T0r-T0l)
+    #
+    # plt.figure(2)
+    # plt.plot(T_x)
 
     # plt.figure(2)
     # plt.plot(m)
 
-
-
     # plt.figure(2)
     # plt.plot(x, ms)
-
-
 
 
 def problem_4_1():
     P = np.linspace(0.1, 10, 1000)
     atm_to_pa = 101325
     pa_to_kpa = 0.001
-    P = P*atm_to_pa
-    T = np.linspace(273.15, 100+273.15, 1000)
+    P = P * atm_to_pa
+    T = np.linspace(273.15, 100 + 273.15, 1000)
 
-    R = 287.058   #J/kgK
+    R = 287.058  # J/kgK
 
-    rho = P/(R*T)
+    rho = P / (R * T)
 
     fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
     X = T
     Y = P
     X, Y = np.meshgrid(X, Y)
-    Z = (Y/(R*X))
+    Z = (Y / (R * X))
 
-    surf = ax.plot_surface(X, Y*0.001, Z, cmap=cm.coolwarm)
+    surf = ax.plot_surface(X, Y * 0.001, Z, cmap=cm.coolwarm)
     plt.xlabel('temperature (kelvin)')
     plt.ylabel('pressure (kPa)')
     fig.colorbar(surf, label='density(kg/m^3)')
@@ -91,17 +109,17 @@ def problem_4_1():
     # plt.savefig(os.path.join(outpath, 'p41_3dsurf.png'))
     # plt.show()
 
-    rhonot = 1.17       #ref density kg/m^3
-    ratios = rho/rhonot
-    alphanot = 0.0034      #K^-1
-    betanot = 0.00001       # Pa^-1
-    Pnot = 100000           # Pa
-    Tnot = 298.15           #kelvin
+    rhonot = 1.17  # ref density kg/m^3
+    ratios = rho / rhonot
+    alphanot = 0.0034  # K^-1
+    betanot = 0.00001  # Pa^-1
+    Pnot = 100000  # Pa
+    Tnot = 298.15  # kelvin
 
     # print(ratio)
-    linapproxrho = rhonot - (rhonot*alphanot*(T - Tnot)) + (rhonot*betanot*(P - Pnot))
+    linapproxrho = rhonot - (rhonot * alphanot * (T - Tnot)) + (rhonot * betanot * (P - Pnot))
     print(linapproxrho)
-    larho_ratios = linapproxrho/rhonot
+    larho_ratios = linapproxrho / rhonot
 
     tr = []
     pr = []
@@ -120,8 +138,13 @@ def problem_4_1():
     print('min temp, pressure = ', mtr, 'and', mpr)
     print('max temp, pressure = ', mxtr, 'and', mxpr)
 
+
 def main():
     problem_4_3()
+
+    outpath = r'\\agustin\homes\mfichera\My Documents\_phd\classes\hyd508_flowtransport\homework_exercises\HW4'
+    # for i in np.arange(however many figs)
+    #     plt.savefig(os.path.join(outpath, 'p4_fig().png'.format(i)))
 
 
 if __name__ == '__main__':
